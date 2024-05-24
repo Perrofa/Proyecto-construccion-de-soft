9e567811794fbe2a6fab2f40361af8a31741e545
El3Du4Rd0 <a01710168@tec.mx>
2024-05-24 10:18:37 -0600

    ejs appix interfaces

--- ProyectoMVC/controllers/appix.controller.js
+++ ProyectoMVC/controllers/appix.controller.js
@@ -3,17 +3,17 @@ module.exports.sign_in = async(req, res) =>{
     res.render('./sign_in/sign_in_html')
 }
 module.exports.inicio = async(req, res) =>{
-    res.render('./inicio/inicio_html')
+    res.render('./appix/inicio_html')
 }
 module.exports.perfil = async(req, res) =>{
-    res.render('./perfil/perfil_html')
+    res.render('./appix/perfil_html')
 }
 module.exports.proyectos = async(req, res) =>{
-    res.render('./proyectos/proyectos_html')
+    res.render('./appix/proyectos_html')
 }
 module.exports.riesgos = async(req, res) =>{
-    res.render('./riesgos/riesgos_html')
+    res.render('./appix/riesgos_html')
 }
 module.exports.ayuda = async(req, res) =>{
-    res.render('./ayuda/ayuda_html')
+    res.render('./appix/ayuda_html')
 }
